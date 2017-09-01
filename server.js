3f474c2c96410ac81ba2814535173f4bcff2eeca
rangaraju29139 <30431968+rangaraju29139@users.noreply.github.com>
2017-09-01 22:28:02 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -15,6 +15,12 @@ app.get('/',function (req,res){
 app.get('/article-one',function(req,res){
    res.send('this is article one'); 
 });
+app.get('/article-two',function(req,res){
+   res.send('this is a article two'); 
+});
+app.get('/article-three',function(req,res){
+   res.send('this is a article three'); 
+});
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
