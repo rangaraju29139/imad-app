5312b94f2bc7f73eee83b146c96e87ec9b1e6ef2
rangaraju29139 <30431968+rangaraju29139@users.noreply.github.com>
2017-09-02 12:00:35 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,9 +1,37 @@
 console.log('Loaded!');
- var counter=0;
+
+ /*var counter=0;
  var button=document.getElementById('counter');
  button.onclick= function(){
      
    counter=counter+1;
    var span=document.getElementById('count');
    span.innerHTML=counter.toString();
+ };*/
+ 
+ var button=document.getElementById('counter');
+ button.onclick =  function() {
+   var request = XMLHttpRequest();
+   
+   request.onreadystatechange=function(){
+     if(request.readyState === XMLHttpRequest.DONE){
+         
+         if(request.status === 200){
+             
+             var counter= request.responseText;
+             var span =document.getElementById('counter');
+             span.innerHTML = counter.toString();
+         }
+         
+     }  
+     
+       
+   };
+   
+   //making a request
+   
+request.open('get','http://rangaraju29139.imad.hasura.io/counter',true);
+     request.send(null);
+     
+     
  };
